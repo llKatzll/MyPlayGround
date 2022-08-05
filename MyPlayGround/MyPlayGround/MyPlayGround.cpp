1850efc5de6f308b171447c37ffe245de5f1d729
llKatzll <bluedia0113@gmail.com>
2022-08-05 20:56:17 +0900

    Sleep?
    a mimir

--- MyPlayGround/MyPlayGround/MyPlayGround.cpp
+++ MyPlayGround/MyPlayGround/MyPlayGround.cpp
@@ -1,8 +1,13 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //시간은 *1000 하도록
+
+    cout << "놀이터에 온걸 환영한다 그지 깽깽아!" << endl;
+    Sleep(2000);
+    cout << "test end" << endl;
 }
 
