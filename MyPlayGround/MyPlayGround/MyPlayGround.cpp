dfaed4bf523d196a2bddcf6fd90a29e72681f943
llKatzll <bluedia0113@gmail.com>
2022-08-09 21:15:00 +0900

    PlayGround1
    me-king motho al- omelettoos here jack

--- MyPlayGround/MyPlayGround/MyPlayGround.cpp
+++ MyPlayGround/MyPlayGround/MyPlayGround.cpp
@@ -2,12 +2,153 @@
 #include <Windows.h>
 using namespace std;
 
+//내 놀이터다! 아무거나 막 써! 아무거나 막 커밋해!
+
+//시간은 *1000 하도록
+string _playerName;
+
+void myFriendsName() {
+
+    cout << "놀이터의 입구에 온걸 환영한다 그지 깽깽아!" << endl << endl;
+    
+    Sleep(2000);
+    cout << "네 이름을 입력해라!" << endl << endl;
+    Sleep(500);
+
+    cin >> _playerName;
+
+    cout << "네 이름이.." + _playerName + "이라고?" << endl << endl;
+    Sleep(3000);
+
+    if (_playerName == "캔디") {
+        Sleep(1000);
+        cout << "ㅁ..뭣? 캔디이?" << endl << endl;
+        Sleep(2000);
+        cout << "....." << endl << endl;
+        Sleep(1570);
+        cout << "157?" << endl << endl;
+        Sleep(2000);
+        cout << "L + don’t care + didn’t ask + cry about it + who asked + stay mad + get real + bleed + mald seethe cope harder + dilate + incorrect + hoes mad + pound sand + basic skill issue + typo + ur dad left + you fell off + no u + the audacity + triggered + repelled + ur a minor + k. + any askers + get a life + ok and? + cringe + copium + go outside + touch grass + kick rocks + quote tweet + think again + not based + not funny didn’t laugh + social credits -999, 999, 999, 999 + get good + reported + ad hominem + ok boomer + small pp + ur allergic to sunlight + GG! + get rekt + trolled + your loss + muted + banned + kicked + permaban + useless + i slept with ur mom + yo momma + yo momma so fat + redpilled + no bitches allowed + i said it better + tiktok fan + get a life + unsubscribed + plundered + go tell reddit + donowalled + simp + get sticked bug LOL + talk nonsense + trump supporter + your’re a full time discord mod + you’re* + grammar issue + nerd + get clapped + kys + lorem ipsum dolor sit amet + go outside + bleach + lol + gay + retard + autistic + reported + ask deez + ez clap + straight cash + idgaf + ratio again + stay mad + read FAQ + youre lost + you “re” + stay pressed" << endl;
+        cout << "+ you failed kindergarten + rickrolled + no lifer + guten freunden schickt man einen deutschen panzer + you have a anime profile picture + an* + fatherless + motherless + sisterless + brotherless + orphan + you can't catch this ratio + catch some bitches + I don't care about your opinion + genshin player + you dress like garbage + 日本語がお上手ですね + get fucked + you can’t understand what the word intelligence means with your dumb ass + you have hair + queued + put some thought into what you're going to do with that + stfu + go to bed + yes, i'm taller than you + i think your joke is funny + i rejected your mother's advances + marooned + you can’t read + I win + final ratio you fell off + ratio + you're white + you're british + who asked + no u + deez nuts + radio + don't care + didn't ask + i'm a minor + i'm neurodivergent + caught in 4k + cope + seethe + GG + in 1947 the world's first general purpose computer, the 30 ton ENIAC was created + your mom's + the hood watches markiplier now + grow up + L + L (part 2) + retweet + ligma + taco bell tortilla crunch + think outside the bun + ur benched + ur a wrench + i own you + ur dad fell off + my dad could beat ur dad up + ur aimhacking + silver elite + tryhard + boomer + sksksksk + ur beta + i'm sigma + ur submissive + L (part 3) + yb better + ur sus + this is a cry for help and i'm extremely depressed. + quote tweet + you're cringe + i did your mom + you bought monkey nft + you're weirdchamp + you're a clown + my father left me at the age of 4 and i never recovered since + my dad owns steam + who want me? + i'm lonely + they didn't think it could possibly happen, but they're releasing L + what is this + L + ratio + wrong + get a job + unfunny + you fell off + never liked you anyway + cope + ur allergic to gluten + don't care + cringe ur a kid + literally shut the fuck up + galileo did it better + your avi was made in MS Excel + ur bf is kinda ugly + i have more subscribers + owned" << endl;
+        cout << "reverse double take back + pedophile + cancelled + done for + don't give a damn + get a job + sus + baka + sussy baka + get blocked + mad free + freer than air + furry + rip bozo + you're a (insert stereotype) + slight_smile + aired + cringe again + Super Idol的笑容 + mad cuz bad + my pronouns are xe, xem & xyr + irrelevant + deal with it + screencapped your bio + karen/kyle + jealous + you're deaf + balls + i'll be right back + go ahead whine about it + not straight + eat paper + you lose + count to three + your problem + no one cares + log off + don't care even more + sex offender + sex defender + get religion + not okay + glhf + NFT owner + you make bad memes + problematic + fall in line + dog water + you look like a wall + you don’t know 2 + 2 with yo head ass + you are going to my cringe compilation + you can’t count to five + try again" << endl;
+
+        return;
+    }
+    else if (_playerName == "카츠") {
+        Sleep(2000);
+        cout << "주인장 나리 오셨습니까.." << endl << endl;
+        Sleep(1000);
+        cout << "크흠.." << endl << endl;
+        Sleep(1000);
+        cout << "설명은 스킵하겠습니다.." << endl << endl;
+        Sleep(2000);
+    }
+    else {
+        Sleep(500);
+        cout << "놀이터에 온 걸 환영한다 그지깽깽아!" << endl << endl;
+        Sleep(1000);
+        cout << "난 여길 만든 진행자 카츠 다!" << endl << endl;
+        Sleep(1200);
+        cout << "이제부터, 앞으로도! 매일 여길 이상한 곳으로 만들거다!" << endl << endl;
+        Sleep(3000);
+        cout << "그 전에 점점이의 게임채널 구독좀" << endl << endl;
+        Sleep(2000);
+        cout << "(참고로 이미 사라진 채널임)" << endl << endl;
+        Sleep(1500);
+        cout << "ㅋㅋㅋ" << endl << endl;
+        Sleep(1000);
+    }
+}
+void openDoorPlayGround() {
+    cout << endl;
+    Sleep(2000);
+    cout << "이제 놀러가도 된다!" << endl << endl;
+    Sleep(2000);
+    cout << "(진행자는 미소를 지으며 놀이터의 문을 열어준다.)" << endl << endl;
+    Sleep(5000);
+    cout << "=================================================" << endl << endl;
+}
+
+void howToplay() {
+    
+    string a;
+
+    cout << endl;
+    cout << "=================================================" << endl << endl;
+    cout << endl;
+    Sleep(1000);
+    cout << "플레이 방식은 간단하다!" << endl << endl;
+    Sleep(1500);
+    cout << "텍스트로 이루어진 선택지를 그대로 똑같이 따라쓰면 된다." << endl << endl;
+    Sleep(2000);
+    cout << "간단히 테스트 해보지!" << endl << endl;
+    Sleep(1500);
+    cout << "내가 입력한 단어를 그대로 따라 쳐봐!" << endl << endl;
+    Sleep(2000);
+    cout << "=================================================" << endl << endl;
+    Sleep(1000);
+    cout << ">>아리혜" << endl << endl;
+    Sleep(1000);
+    cin >> a;
+    Sleep(2000);
+    if (a == "아리혜") {
+        cout << endl;
+        cout << "그래! 그렇게 하는거야" << endl << endl;
+        openDoorPlayGround();
+    }
+    else if (a == ">>아리혜") {
+        cout << "굳이 앞에 >> 까지 붙일 필욘 없다!" << endl;
+        Sleep(1000);
+        cout << ">> 앞에 있는 단어만 쳐줘!" << endl << endl;
+        Sleep(2000);
+        cout << "오케이!" << endl << endl;
+        openDoorPlayGround();
+    }
+    else {
+        Sleep(1000);
+        cout << endl;
+        cout << "하.. 그거 그렇게 하는거 아닌데" << endl << endl;
+        Sleep(2000);
+        cout << "이번만 봐드립니다.." << endl << endl;
+        openDoorPlayGround();
+    }
+
+}
+
+void loading() {
+    Sleep(2000);
+    cout << "=================================================" << endl << endl;
+    Sleep(1500);
+    cout << "=================================================" << endl << endl;
+    Sleep(1000);
+    cout << "=================================================" << endl << endl;
+    Sleep(500);
+    cout << "=================================================" << endl << endl;
+    Sleep(400);
+    cout << "=================================================" << endl << endl;
+    Sleep(300);
+    cout << "=================================================" << endl << endl;
+    Sleep(200);
+    cout << "=================================================" << endl << endl;
+    Sleep(100);
+    cout << "=================================================" << endl << endl;
+}
+
 int main()
 {
-    //시간은 *1000 하도록
+    myFriendsName();
 
-    cout << "놀이터에 온걸 환영한다 그지 깽깽아!" << endl;
-    Sleep(2000);
-    cout << "test end" << endl;
+    if (_playerName != "카츠") {
+        howToplay();
+    }
+
+    loading();
+
+
+
+
+    
+
 }
+
 
